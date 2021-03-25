ab9b3a6deb3a8c163717856855d21f4b9d058123
Kalin Roussinov <kalinrouss@gmail.com>
2021-03-25 17:05:25 +0200

    finished the filters and fixed their icons

--- src/Analytics/Analytics.js
+++ src/Analytics/Analytics.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Menu } from 'antd';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { DollarOutlined, HistoryOutlined } from '@ant-design/icons';
 import styles from './analytics.module.scss'
 
 const { SubMenu } = Menu;
@@ -20,27 +20,25 @@ export default function Analytics() {
                 mode="inline"
                 className={styles.analyticsMenu}
             >
-                <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+                <SubMenu key="sub1" icon={<HistoryOutlined />} title="RECORD TYPES">
 
-                    <Menu.Item key="1">Option 1</Menu.Item>
-                    <Menu.Item key="2">Option 2</Menu.Item>
-
-                    <Menu.Item key="3">Option 3</Menu.Item>
-                    <Menu.Item key="4">Option 4</Menu.Item>
+                    <Menu.Item key="1">All Record Types</Menu.Item>
+                    <Menu.Item key="2">Income</Menu.Item>
+                    <Menu.Item key="3">Expense</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-                    <Menu.Item key="5">Option 5</Menu.Item>
-                    <Menu.Item key="6">Option 6</Menu.Item>
-                    <SubMenu key="sub3" title="Submenu">
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
+                <SubMenu key="sub2" icon={<DollarOutlined />} title="Expenses">
+                    <Menu.Item key="4">Food &amp; Drinks</Menu.Item>
+                    <Menu.Item key="5">Shopping</Menu.Item>
+                    <Menu.Item key="6">Vehicle &amp; Transportation</Menu.Item>
+                    <Menu.Item key="7">Communication &amp; PC</Menu.Item>
+                    <Menu.Item key="8">Entertainment &amp; Life</Menu.Item>
+                    <Menu.Item key="9">Investments</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub4" icon={<SettingOutlined />} title="Navigation Three">
-                    <Menu.Item key="9">Option 9</Menu.Item>
-                    <Menu.Item key="10">Option 10</Menu.Item>
-                    <Menu.Item key="11">Option 11</Menu.Item>
-                    <Menu.Item key="12">Option 12</Menu.Item>
+                <SubMenu key="sub4" icon={<DollarOutlined />} title="Income">
+                    <Menu.Item key="10">Salary</Menu.Item>
+                    <Menu.Item key="11">Lottery &amp; Gambling</Menu.Item>
+                    <Menu.Item key="12">Interests &amp; Dividends</Menu.Item>
+                    <Menu.Item key="13">Lending &amp; Renting</Menu.Item>
                 </SubMenu>
             </Menu>
         </>
