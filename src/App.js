7d369f8b030bf283328431183552fd44520263e8
Alexander <akalvachev99@gmail.com>
2021-03-25 11:01:11 +0200

    Refactor navigation process

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import './index.css'
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Navbar from './Navbar/Navbar'
 import LoginPage from './Login/LoginPage'
 import RegisterPage from './Register/RegisterPage'
-// import Records from './Records/Records';
+import HomePage from './Home/HomePage'
+import Records from './Records/Records';
+
 import 'antd/dist/antd.css';
 
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar/>
+        <BrowserRouter>
+          <Navbar />
+
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+
+            <Route path="/records">
+              <Records />
+            </Route>
+
+            <Route path="/analytics">
+            </Route>
+
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+
+            <Route path="/register">
+              <RegisterPage />
+            </Route>
+
+            <Route path="*">
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     </>
   );
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,54 +1,24 @@
 import React from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import LoginPage from '../Login/LoginPage'
-import RegisterPage from '../Register/RegisterPage'
-import HomePage from '../Home/HomePage'
-import Records from '../Records/Records';
+import { Layout, Menu } from 'antd';
+import { Link } from "react-router-dom";
 
-const { Header, Content, Footer } = Layout;
+const { Header } = Layout;
 
 export default function Navbar() {
     return (
         <>
-            <BrowserRouter>
-                <Layout>
-                    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-                        <div className="logo" />
-                        <Menu theme="dark" mode="horizontal">
-                            <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-                            <Menu.Item key="2"><Link to="/records">Records</Link></Menu.Item>
-                            <Menu.Item key="3"><Link to="/analytics">Analytics</Link></Menu.Item>
-                            <Menu.Item key="4"><Link to="/login">Login</Link></Menu.Item>
-                            <Menu.Item key="5"><Link to="/register">Register</Link></Menu.Item>
-                        </Menu>
-                    </Header>
-                </Layout>
-
-                <Switch>
-                    <Route exact path="/">
-                        <HomePage />
-                    </Route>
-
-                    <Route path="/records">
-                        <Records/>
-                    </Route>
-
-                    <Route path="/analytics">
-                    </Route>
-
-                    <Route path="/login">
-                        <LoginPage />
-                    </Route>
-
-                    <Route path="/register">
-                        <RegisterPage />
-                    </Route>
-
-                    <Route path="*">
-                    </Route>
-                </Switch>
-            </BrowserRouter>
+            <Layout>
+                <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal">
+                        <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+                        <Menu.Item key="2"><Link to="/records">Records</Link></Menu.Item>
+                        <Menu.Item key="3"><Link to="/analytics">Analytics</Link></Menu.Item>
+                        <Menu.Item key="4"><Link to="/login">Login</Link></Menu.Item>
+                        <Menu.Item key="5"><Link to="/register">Register</Link></Menu.Item>
+                    </Menu>
+                </Header>
+            </Layout>
         </>
     )
 }
