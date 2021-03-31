6c83a8af84b07219a6b6c56f5ef8352feb32646c
Alexander <akalvachev99@gmail.com>
2021-03-31 18:22:10 +0300

    Change grid layout from class to function component

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,15 +1,11 @@
 import React, { useEffect } from 'react'
 
-import CardContent from './CardContent'
 import GridLayout from './GridLayout'
-import firebase from '../firebase'
-import { database } from '../firebase'
 
 export default function HomePage() {
     return (
         <>
             <div >
-                <CardContent />
                 <GridLayout />
             </div>
         </>
