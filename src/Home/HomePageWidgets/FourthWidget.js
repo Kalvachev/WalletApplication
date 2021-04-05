2701e562a6b4d338226e42e60b764f7c2d7b71cf
Alexander <akalvachev99@gmail.com>
2021-04-05 21:09:36 +0300

    Work on the widgets

--- src/Home/HomePageWidgets/FourthWidget.js
+++ src/Home/HomePageWidgets/FourthWidget.js
@@ -1,41 +1,29 @@
 import React, { useMemo } from 'react';
 import styles from "../homepage.module.scss"
 import { Line, defaults } from 'react-chartjs-2';
-
+import { Card } from 'antd';
 
 defaults.global.tooltips.enabled = true;
 defaults.global.legend.position = 'bottom'
 
 export default function FourthWidget({ bills }) {
+    const allExpenses = Math.abs(bills.filter(data => data.type === "expense").reduce(((acc, curr) => acc + Number(curr.amount)), 0))
+    const allIncomes = bills.filter(data => data.type === "income").reduce(((acc, curr) => acc + Number(curr.amount)), 0);
+
     return (
         <div>
-            <Line className={styles.recordsCenterGraphLinear}
-                data={{
-                    labels: Array.from(new Set(bills.map(d => d.date))),
-                    datasets: [
-                        {
-                            label: "My total balance",
-                            fill: false,
-                            lineTension: 0.1,
-                            backgroundColor: "rgba(75, 192, 192, 0.4)",
-                            borderColor: "rgba(75, 192, 192, 1)",
-                            borderCapStyle: 'butt',
-                            borderDash: [],
-                            borderDashOffset: 0.0,
-                            borderJoinStyle: 'miter',
-                            pointBorderColor: "rgba(75,192,192,1)",
-                            pointBackgroundColor: "#fff",
-                            pointBorderWidth: 1,
-                            pointHoverRadius: 5,
-                            pointHitRadius: 10,
-                            data: bills.map(d => d.amount),
-                        }
-                    ]
-                }}
-                options={{
-                    maintainAspectRatio: false,
-                }}
-            />
+            <div className={styles.fourthWidgetChartContainer}>
+                <div className={styles.middleGraphContainer}>
+                    <div className={styles.middleGraphContainerTopPart}>
+                        <h2>Wallet Application</h2>
+                    </div>
+
+                    <div className={styles.middleGraphContainerCenterPart}>
+                        <h2>Current balance: {Number(allIncomes - allExpenses)}$</h2>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
